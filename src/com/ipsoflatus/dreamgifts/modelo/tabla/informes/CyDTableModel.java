8bb343ea74e5f6e550efdd90a087f0c2b6ee2f83
Pablo Soto <sotoipablo@gmail.com>
2021-08-12 22:12:15 -0400

    Informe devoluciones actualizacion

--- src/com/ipsoflatus/dreamgifts/modelo/tabla/informes/CyDTableModel.java
+++ src/com/ipsoflatus/dreamgifts/modelo/tabla/informes/CyDTableModel.java
@@ -1,14 +1,28 @@
 package com.ipsoflatus.dreamgifts.modelo.tabla.informes;
 
+import com.ipsoflatus.dreamgifts.modelo.entidad.EstadoVenta;
 import com.ipsoflatus.dreamgifts.modelo.entidad.Venta;
+import com.ipsoflatus.dreamgifts.modelo.servicio.EVService;
 import com.ipsoflatus.dreamgifts.modelo.tabla.ventas.ActualizacionDespachoTableModel;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CyDTableModel extends ActualizacionDespachoTableModel {
 
     @Override
     public void actualizar(List<Venta> items) {
-        super.actualizar(items); //To change body of generated methods, choose Tools | Templates.
+   EstadoVenta devolucion = obtenerEstado();
+        if (devolucion != null) {
+            ventas = items.stream().filter(v -> v.getEstadoVenta().equals(devolucion)).collect(Collectors.toList());
+            fireTableDataChanged();
+    }
+    
+}
+    private EstadoVenta obtenerEstado() {
+        List<EstadoVenta> result = EVService.getInstance().buscar();
+        if (result.isEmpty())
+            return null;
+        return EVService.getInstance().buscar().get(3);
     }
     
 }
